910db76c48490a9ac1a68416d448786ea573cb3e
akitanak <akihito_tanaka@me.com>
2020-11-13 23:21:37 +0900

    lintエラーの解消

--- try_fastapi/__main__.py
+++ try_fastapi/__main__.py
@@ -2,10 +2,8 @@ import os
 import sys
 
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
-
-import uvicorn
-
-from try_fastapi.api import app
+import uvicorn  # noqa E402
+from try_fastapi.api import app  # noqa E402
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=int(os.getenv("PORT", "8000")))
