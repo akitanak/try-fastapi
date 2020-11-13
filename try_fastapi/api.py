611fb1c6b1ef850225bf59982ef93c3c5a3d4184
akitanak <akihito_tanaka@me.com>
2020-11-13 23:07:21 +0900

    post と get のAPIを作り、OpenAPIのドキュメントで確認できた。

--- try_fastapi/api.py
+++ try_fastapi/api.py
@@ -1,11 +1,12 @@
 from datetime import date, datetime
 from typing import List, Optional
+from uuid import UUID
 
 from fastapi import FastAPI
 from pydantic import BaseModel
 
 from try_fastapi.applications.tasks import TaskService
-from try_fastapi.domains.entities.tasks import Task, Priority
+from try_fastapi.domains.entities.tasks import Priority
 from try_fastapi.modules import task_service
 
 app = FastAPI()
@@ -14,30 +15,22 @@ app = FastAPI()
 app_service: TaskService = task_service
 
 
-class AddTaskReq(BaseModel):
+class AddTaskRequest(BaseModel):
     task_name: str
     due_date: Optional[date]
     priority: Optional[Priority]
 
 
-class TaskRes(BaseModel):
-    id: str
+class TaskResponse(BaseModel):
+    id: UUID
     task_name: str
     due_date: Optional[date]
     priority: Optional[Priority]
     is_done: bool
-    created_at: Optional[datetime]
+    created_at: datetime
 
-    @classmethod
-    def to_response(cls, task: Task):
-        return TaskRes(
-            id=str(task.id),
-            task_name=task.task_name,
-            due_date=task.due_date,
-            priority=task.priority.value if task.priority is not None else None,
-            is_done=task.is_done,
-            created_at=task.created_at,
-        )
+    class Config:
+        orm_mode = True
 
 
 @app.get("/healthz")
@@ -45,13 +38,13 @@ def healthz():
     return {"status": "I'm Healthy!"}
 
 
-@app.post("/tasks/", response_model=TaskRes)
-def add(add_req: AddTaskReq) -> TaskRes:
+@app.post("/tasks/", response_model=TaskResponse)
+def add(add_req: AddTaskRequest) -> TaskResponse:
     task = app_service.add(add_req.dict())
-    return TaskRes.to_response(task)
+    return TaskResponse.from_orm(task)
 
 
-@app.get("/tasks/")
-def list(response_model=List[TaskRes]) -> List[TaskRes]:
+@app.get("/tasks/", response_model=List[TaskResponse])
+def list() -> List[TaskResponse]:
     tasks = app_service.list()
-    return [TaskRes.to_response(task) for task in tasks]
+    return [TaskResponse.from_orm(task) for task in tasks]
